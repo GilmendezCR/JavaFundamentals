a8f37a5d39a31c762830f7f2423ba0c3a3ab2d8d
GilmendezCR <43428370+GilmendezCR@users.noreply.github.com>
2023-08-17 16:43:43 -0600

    Add files via upload

---
+++ JavaFundamentalsLearning/myInitials.java
@@ -0,0 +1,18 @@
+
+public class myInitials
+{
+public static void main(String[] args) {
+
+System.out.print(" G G G " + "     S S S");
+System.out.print("\n");
+System.out.println("G      G" + "  S      S");
+System.out.println("G" + "         S");
+System.out.println("G  G G" + " G" + "   S S S");
+System.out.println("G     " + " G" + "        S");
+System.out.println("G     " + " G" + "  S     S");
+System.out.println(" G G G " + "    S S S" );
+
+
+       
+}
+}
