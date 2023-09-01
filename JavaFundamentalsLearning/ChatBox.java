72cb894c006d2fa00c2551f30f01be6beb18f81f
Ossus <ossus@Ossuss-MacBook-Pro.local>
2023-09-01 10:52:37 -0600

    Adding a space in line of code

--- JavaFundamentalsLearning/ChatBox.java
+++ JavaFundamentalsLearning/ChatBox.java
@@ -11,12 +11,12 @@ String Name = scanner.nextLine();
 System.out.print("Hi" + " " + Name + " " + "I'm Javabot. Where are you from?:");
 String Home = scanner.nextLine();
 
-System.out.print("I hear it's beutiful" + " " + Home + "I'm from a place called Oracle. How old are you?:");
+System.out.print("I hear it's beutiful" + " " + Home + " " + "I'm from a place called Oracle. How old are you?:");
 int Age = scanner.nextInt();
 
 System.out.print("So you are" + " " + Age + " " + " so cool! I am 400 years old");
 
-
+scanner.close();
 
 
 
