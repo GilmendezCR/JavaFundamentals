922b4d5636398d09eadf52ae9c0b35148601cdb5
gilmendezcr <gilberto-247@hotmail.com>
2023-09-05 16:13:32 -0600

    Cleaning code

--- JavaFundamentalsLearning/myInitials.java
+++ JavaFundamentalsLearning/myInitials.java
@@ -3,8 +3,7 @@ public class myInitials
 {
 public static void main(String[] args) {
 
-System.out.print(" G G G " + "     S S S");
-System.out.print("\n");
+System.out.println(" G G G " + "     S S S");
 System.out.println("G      G" + "  S      S");
 System.out.println("G" + "         S");
 System.out.println("G  G G" + " G" + "   S S S");
